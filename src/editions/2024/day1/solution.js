b97e53370db02aed1e79dcd7d9800cc61b05287f
irian <irian.code@hotmail.com>
2024-12-02 19:43:35 +0100

    Solved part 2

--- src/editions/2024/day1/solution.js
+++ src/editions/2024/day1/solution.js
@@ -40,4 +40,34 @@ function part1() {
   return solution;
 }
 
+function part2() {
+  const {list1, list2} = inputToArrays(problemInput);
+
+  // console.log({list1, list2});
+
+  const list2Map = (() => {
+    const map = new Map();
+
+    for (let i = 0; i < list2.length; i++) {
+      const val = list2[i];
+
+      if (map.has(val)) {
+        map.set(val, map.get(val) + 1);
+      } else {
+        map.set(val, 1);
+      }
+    }
+
+    return map;
+  })();
+
+  const solution = list1.reduce(
+    (acc, val) => acc + val * (list2Map.get(val) ?? 0),
+    0
+  );
+
+  return solution;
+}
+
 console.log('Part 1 solution: ', part1());
+console.log('Part 2 solution: ', part2());
